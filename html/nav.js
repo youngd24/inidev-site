8acff978b61251283c9722194cc0e9c951d6557f
Darren Young <youngd24@gmail.com>
2025-08-19 10:43:11 -0500

    started adding common nav

---
+++ html/nav.js
@@ -0,0 +1,15 @@
+// INITECH Navigation Menu - IE5 Compatible
+// This function writes the navigation bar HTML using document.write()
+
+function writeNavigation() {
+  document.write('<div class="nav">');
+  document.write('<a href="/">Home</a>');
+  document.write('<a href="/xfiles.html">Files</a>');
+  document.write('<a href="/support.html">Support</a>');
+  document.write('<a href="/services.html">Services</a>');
+  document.write('<a href="/phones.html">Phones</a>');
+  document.write('<a href="/contact.html">Contact</a>');
+  document.write('<a href="/about.html">About</a>');
+  document.write('<a href="/inigame.html">INIGAME</a>');
+  document.write('</div>');
+}
