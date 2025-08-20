f519dfc24e8ed298830ca2887318e8f4e1a222f5
Darren Young <youngd24@gmail.com>
2025-08-20 10:59:50 -0500

    added files

--- html/footer.js
+++ html/footer.js
@@ -1,6 +1,6 @@
 function addFooter() {
     var footerDiv = document.createElement('div');
-    footerDiv.innerHTML = '<p align="center" style="margin-top: 20px; padding: 10px; border-top: 1px solid #ccc;">Contents Copyright (c) 1999, INITECH Corp</p>';
+    footerDiv.innerHTML = '<br><br><br><p align="center" style="margin-top: 20px; padding: 10px; border-top: 1px solid #ccc;">Contents Copyright (c) 1999-2099, INITECH Corp<br><br><i>Site Created using ChatGPT, Cursor and Claude Code on a Commodore 64</i></p>';
     document.body.appendChild(footerDiv);
 }
 
