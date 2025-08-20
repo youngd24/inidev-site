fb308ae56fe9d9e4960de6cbbd282fa26eab1696
Darren Young <youngd24@gmail.com>
2025-08-19 21:18:37 -0500

    adjusted nav order

--- html/nav.js
+++ html/nav.js
@@ -4,11 +4,11 @@
 function writeNavigation() {
   document.write('<div class="nav">');
   document.write('<a href="/">Home </a>');
-  document.write('<a href="/xfiles.html">Files </a>');
-  document.write('<a href="/support.html">Support </a>');
   document.write('<a href="/services.html">Services </a>');
+  document.write('<a href="/support.html">Support </a>');
   document.write('<a href="/phones.html">Phones </a>');
   document.write('<a href="/contact.html">Contact </a>');
+  document.write('<a href="/xfiles.html">Files </a>');
   document.write('<a href="/about.html">About </a>');
   document.write('<a href="/inigame.html">INIGAME</a>');
   document.write('</div>');
